865d994b42f327c11c3a87f939d551376bb9a48a
squirlemaster42 <squirlemaster@gmail.com>
2015-09-18 21:49:12 -0400

    Started adding code to extract information from a file

--- LOL Stat Calculator/src/com/jakob/getInfo/GetItemStats.java
+++ LOL Stat Calculator/src/com/jakob/getInfo/GetItemStats.java
@@ -1,8 +1,24 @@
 package com.jakob.getInfo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class GetItemStats {
 
+	@SuppressWarnings("resource")
 	public void getItemStats(String fileName) {
-		
+		try {
+			File myFile = new File(fileName);
+			FileReader fileReader = new FileReader(myFile);
+			BufferedReader reader =  new BufferedReader(fileReader);
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+				//Add case to look for info
+			}
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
--- LOL Stat Calculator/src/com/jakob/getInfo/GetRuneStats.java
+++ LOL Stat Calculator/src/com/jakob/getInfo/GetRuneStats.java
@@ -1,8 +1,24 @@
 package com.jakob.getInfo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class GetRuneStats {
 
+	@SuppressWarnings("resource")
 	public void getRuneInfo(String fileName) {
-		
+		try {
+			File myFile = new File(fileName);
+			FileReader fileReader = new FileReader(myFile);
+			BufferedReader reader =  new BufferedReader(fileReader);
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+				//Add case to look for info
+			}
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
